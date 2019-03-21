f47982bf4e1dac1f8e1991a248c668dac3f351ce
Earle-Poole <eepoole4@gmail.com>
2019-03-20 23:52:17 -0500

    fixed broken sources

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    	<img alt="Vue logo" src="./assets/logo.png">
+    	<img alt="Vue logo" src="https://vuejs.org/images/logo.png">
 	  <div id="SearchAndClock">
 		<SearchBar />
 		<vue-clock />
